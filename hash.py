53597a593ea32f2d25a1417ed3ee29f93c964fab
GuillaumeG13 <guillaume_gay@ymail.com>
2019-02-27 16:30:18 +0100

    sha256 fonctionnel

--- hash.py
+++ hash.py
@@ -1,8 +1,10 @@
+## SHA-256 test with hashlib
+## source : https://medium.com/@dwernychukjosh/sha256-encryption-with-python-bf216db497f9
+## tested by Guillaume Gay
+
 import hashlib
 
 def encrypt_string(hash_string):
     sha_signature = \
         hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
-
-print(encrypt_string("Workshop pls"))
