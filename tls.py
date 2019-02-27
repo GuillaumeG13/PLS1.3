cf416ff1c906d3536a65f1adb886ce1beda616f9
Unknown <yoann.dequeker@etu.emse.fr>
2019-02-27 09:24:11 +0100

    Commit for Elsa

--- tls.py
+++ tls.py
@@ -1,12 +1,7 @@
 from tls_socket import *
-from math import floor
-from hash import encrypt_string
-from hkdf import *
-from binascii import unhexlify
 import subprocess
-
-
-
+import hashlib
+from binascii import unhexlify
 
 class TLS:
 	def __init__(self, curve, serveur=True):
@@ -14,12 +9,11 @@ class TLS:
 		self.callback = None
 		self.socket = SocketTLS(ip="127.0.0.1", port=1799, server=False, workers=5)
 		self.messageHelloList = []
-		self.server_key_exchange = ""
-		self.server_handshake_key = ""
+
 		self.public_key = ""
 		self.private_key = ""
 		self.external_key = ""  # It's the key related to the other point of connection
-		self.secret = ""
+		self.secret = ""        # Secret shared through DeffieHellman
 		self.serveur = serveur
 
 		self.client_handshake_iv = ""
@@ -28,9 +22,10 @@ class TLS:
 		self.server_handshake_key = ""
 		self.handshake_secret = ""
 
+	def initialize_connection(self):
+		self.socket.initialize_connection()
 
 	def hello(self, params):
-
 		random = params['random']
 		version = params['version']
 		session_id = params['session_id']
@@ -58,6 +53,19 @@ class TLS:
 		msg = self.socket.receive()
 		self.messageHelloList.append(msg[5:])
 
+	def verify_certificates(self):
+		# TODO
+		pass
+
+	def generate_asymetrique_keys(self):
+		# TODO : Maxime & Marcou
+		self.private_key = ""
+		self.public_key = ""
+
+	def publish_public_key(self):
+		# TODO
+		pass
+	
 	def receive_external_key(self):
 		"""Publish and get public keys"""
 		if self.serveur:
@@ -69,22 +77,23 @@ class TLS:
 			self.socket.update(self.public_key)
 			self.socket.send()
 
-	def server_handshake_key_generation(self):
+	def handshake_key_generation(self):
 		# Multiplication courbe ECC
 		# TODO : Maxime et Marcou
 		# self.receive_external_key()
-
 		# self.secret = hex(int(self.private_key, 16) * int(self.external_key, 16)).split('0x')[1]
-		# hello_hash = encrypt_string("".join(self.messageHelloList))
+
+		#self.messageHelloList = ["16030100ca010000c60303000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff0006130113021303010000770000001800160000136578616d706c652e756c666865696d2e6e6574000a00080006001d00170018000d00140012040308040401050308050501080606010201003300260024001d0020358072d6365880d1aeea329adf9121383851ed21a28e3b75e965d0d2cd166254002d00020101002b0003020304", "160303007a020000760303707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f20e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff130100002e00330024001d00209fd7ad6dcff4298dd3f96d5b1b2af910a0535b1488d7f8fabb349a982880b615002b00020304"]
+		#hello_hash = hashlib.sha256(unhexlify("".join(self.messageHelloList))).hexdigest()
+		#print("Hello hash : " + hello_hash)
 		hello_hash = "da75ce1139ac80dae4044da932350cf65c97ccc9e33f1e6f7d2d4b18b736ffd5"
 		keys = subprocess.check_output("windaube " + hello_hash + " " + self.secret, shell=True)
 		[self.client_handshake_key, self.server_handshake_key, self.client_handshake_iv, self.server_handshake_iv] = keys.decode().split('plop')[1].split(' ')[1:-1]
 		return self.client_handshake_key, self.server_handshake_key, self.client_handshake_iv, self.server_handshake_iv
 
+	def handshake_encryption(self):
 
 
-	def initialize_connection(self):
-		self.socket.initialize_connection()
 
 	@staticmethod
 	def format_length(length, size):
